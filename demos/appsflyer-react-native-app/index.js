e50b9931df3fd64550f98e8a61a28ba60dc72625
Diogo Ratto <diogoratto.dev@gmail.com>
2024-11-14 16:33:03 -0300

    Update index.js

--- demos/appsflyer-react-native-app/index.js
+++ demos/appsflyer-react-native-app/index.js
@@ -5,5 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+//Custom Code
+import appsFlyer from 'react-native-appsflyer';
 
 AppRegistry.registerComponent(appName, () => App);
